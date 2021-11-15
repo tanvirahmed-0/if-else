c81c527ea53e232927a1da4b7379626f1a45ae08
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-11-15 16:25:14 +0600

    Add files via upload

---
+++ Looooooop/script.js
@@ -0,0 +1,24 @@
+var num = 10;
+
+if(num % 2 == 0)
+console.log("Even");
+
+else
+console.log("Odd");
+
+var num = prompt("Enter the value");
+           
+    if(num>=80){
+    document.write("A+");
+    
+    }
+    else if(num>=70){
+    document.write("A");
+    
+    }
+    else if(num>=60){
+        document.write("A-");
+        
+    }
+    else
+    document.write("Fail")
